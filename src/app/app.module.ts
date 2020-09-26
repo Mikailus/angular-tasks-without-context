7932627297513681e1a0c3183075a86acd619f1e
Michał Wajer <wajer.michael@gmail.com>
2020-09-26 15:11:30 +0200

    Wydzielanie nagłówka, głównej sekcji, sekcji pobocznej i stopki

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent,
+    MainSectionComponent
   ],
   imports: [
     BrowserModule
