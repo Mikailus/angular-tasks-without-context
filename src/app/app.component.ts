44793af9be3460b7bef34e70e3882288da06bdba
Michał Wajer <wajer.michael@gmail.com>
2020-09-26 23:23:56 +0200

    Obliczanie przy użyciu router events ile trwało przenawigowanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd, NavigationCancel, NavigationError, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private startTime: number;
+  private stopTime: number;
+  public time: number;
+
+  constructor(private router: Router) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkEvent(routerEvent);
+    });
+  }
+
+  private checkEvent(routerEvent : Event) : void {
+    if (routerEvent instanceof NavigationStart) {
+      this.startTime = Date.now();
+    }
+ 
+    else if (routerEvent instanceof NavigationEnd ||
+             routerEvent instanceof NavigationCancel ||
+             routerEvent instanceof NavigationError) {
+      this.stopTime = Date.now();
+      this.time = this.stopTime - this.startTime;
+    }
+  }
 }
